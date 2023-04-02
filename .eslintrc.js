a28c38a7519bfe8f58cf5b8335a8c28d06b80e4d
Andrew <5755685+arnidan@users.noreply.github.com>
2023-04-02 14:18:10 +0200

    update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  env: {node: true},
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
@@ -9,12 +10,28 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
   ],
   rules: {
+    'no-async-promise-executor': 0,
+    'no-case-declarations': ['warn'],
+    'no-irregular-whitespace': 0,
     'require-await': 1,
     'no-console': 0,
-    'indent': ['error', 2, {'SwitchCase': 1}],
+    'indent': 'off',
+    '@typescript-eslint/indent': [
+      'error',
+      2,
+      {
+        'SwitchCase': 1,
+        'ignoredNodes': [
+          'FunctionExpression > .params[decorators.length > 0]',
+          'FunctionExpression > .params > :matches(Decorator, :not(:first-child))',
+          'ClassBody.body > PropertyDefinition[decorators.length > 0] > .key'
+        ]
+      }
+    ],
     'linebreak-style': ['error', 'unix'],
     'quotes': ['error', 'single'],
     'semi': ['error', 'always'],
+    'prefer-const': ['warn'],
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {'vars': 'all', 'args': 'after-used', 'ignoreRestSiblings': false}
@@ -23,9 +40,9 @@ module.exports = {
       'error',
       {'after': false, 'before': false}
     ],
-    // '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-explicit-any': 0,
     '@typescript-eslint/explicit-module-boundary-types': 0,
-    // '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/ban-ts-comment': ['warn']
-  }
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/ban-ts-comment': 0,
+  },
 };
